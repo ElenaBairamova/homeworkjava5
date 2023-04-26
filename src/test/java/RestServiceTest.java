306035a950c653ee5a0200d2f23c43baa08aa74f
Elena <elshishkanova@yandex.ru>
2023-04-26 22:52:52 +0300

    Change Test

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -1,24 +1,21 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.RestService;
 
 public class RestServiceTest {
-    @Test
-    public void RestCalc() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Rest.csv")
+
+    public void RestCalc(int income, int expense, int threshold, int expected) {
         RestService service = new RestService();
-        int expected = 2;
-        int actual = service.CalcRestService(100_000, 60_000, 150_000);
+
+        int actual = service.CalcRestService(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void RestCalculate() {
-        RestService service = new RestService();
-        int expected = 3;
-        int actual = service.CalcRestService(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
+
